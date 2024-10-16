e5dbe76cea79d45f00173ca13bf23c2f98bb76a4
Utkarsh <utkarsh@Utkarshs-MacBook-Air-7.local>
2024-10-16 21:09:39 +0530

    formatting improved

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -12,35 +12,30 @@ function Orders() {
     const [userinfo] = useRecoilState(userDetailAtom);
     const [consumers, setConsumers] = useState([]);
     const [selectedStatus, setSelectedStatus] = useState({});
-    const [loading, setLoading] = useState(true); // State to manage loading
+    const [loading, setLoading] = useState(true);
 
-    // Options for the delivery status dropdown
     const statusOptions = ["pending", "confirmed", "cancelled"];
 
-    // Function to fetch consumer data from the API when the component loads
     useEffect(() => {
         const token = localStorage.getItem('token');
 
         const config = {
             headers: {
-                Authorization: `Bearer ${token}` // Assuming you are using Bearer token for authentication
+                Authorization: `Bearer ${token}` // Use template literals
             }
         };
 
         axios.get('https://organicspices.azurewebsites.net/api/all-orders', config)
             .then(response => {
-                // Set the fetched consumer data to the state
                 setConsumers(response.data);
-                console.log(response.data);
-                setLoading(false); // Stop loading once the data is fetched
+                setLoading(false);
             })
             .catch(error => {
                 console.error("There was an error fetching the consumers!", error);
-                setLoading(false); // Stop loading in case of an error
+                setLoading(false);
             });
     }, []);
 
-    // Handle status change before submitting
     const handleStatusChange = (userId, productId, newStatus) => {
         setSelectedStatus({
             ...selectedStatus,
@@ -48,7 +43,6 @@ function Orders() {
         });
     };
 
-    // Function to handle updating the delivery status for a specific product
     const updateProductStatus = (userId, productId) => {
         const token = localStorage.getItem('token');
         const newStatus = selectedStatus[`${userId}-${productId}`];
@@ -69,7 +63,6 @@ function Orders() {
 
         axios.put('https://organicspices.azurewebsites.net/api/order_status', payload, config)
             .then(response => {
-                console.log("Status Changed to", response.data);
                 alert("Order Status Changed successfully");
             })
             .catch(error => {
@@ -80,11 +73,11 @@ function Orders() {
     const navigate = useNavigate();
 
     const goToOrders = () => {
-        navigate('/users'); // Navigates to the order list page
+        navigate('/users');
     };
 
     const goToDashboard = () => {
-        navigate('/admin'); // Navigates to the admin dashboard
+        navigate('/admin');
     };
 
     return (
@@ -94,8 +87,8 @@ function Orders() {
                 <div className="m-10 mb-4 font-bold text-3xl">
                     Welcome Admin
                 </div>
-                <div className="flex flex-col md:flex-row">
-                    <div className="m-10 mt-0 w-[200px] h-[200px] flex flex-col gap-10">
+                <div className=" flex-col md:flex-row">
+                    <div className="m-10 mt-0 w-[200px] flex flex-col gap-10">
                         <button 
                             onClick={goToOrders}
                             className="border px-3 py-1 rounded-lg bg-orange-400 font-medium hover:bg-orange-300 text-white">
@@ -107,24 +100,23 @@ function Orders() {
                             Admin Dashboard
                         </button>
                     </div>
-                    <div className="flex-grow p-10 pt-0">
-                        <div className="text-xl mb-5 font-bold">
+                    <div className="flex-grow p-10 pt-0 mb-10 ">
+                        <div className="text-xl mb-5 font-bold mb-10">
                             <h1 className="font-bold">Orders</h1>
                             <div className="font-light text-lg w-100 h-[200px] flex-row gap-10">
-                                <table className="min-w-full table-auto border-collapse border border-gray-300">
+                                <table className="min-w-full table-auto border-collapse rounded-lg mb-10 overflow-hidden shadow-lg">
                                     <thead>
-                                        <tr>
-                                            <th className="px-4 py-2 border border-gray-300">Customer Name</th>
+                                        <tr className="bg-gray-800 text-white">
+                                            <th className="px-4 py-2 border border-gray-300 rounded-tl-lg">Customer Name</th>
                                             <th className="px-4 py-2 border border-gray-300">Date</th>
                                             <th className="px-4 py-2 border border-gray-300">Products & Delivery Status</th>
-                                            <th className="px-4 py-2 border border-gray-300">Total Amount</th>
+                                            <th className="px-4 py-2 border border-gray-300 rounded-tr-lg">Total Amount</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {loading ? (
-                                            // Show skeleton while loading
                                             Array(5).fill().map((_, index) => (
-                                                <tr key={index}>
+                                                <tr key={index} className={`bg-${index % 2 === 0 ? 'white' : 'gray-100'}`}>
                                                     <td className="px-4 py-2 border border-gray-300">
                                                         <Skeleton width={150} />
                                                     </td>
@@ -140,17 +132,17 @@ function Orders() {
                                                 </tr>
                                             ))
                                         ) : (
-                                            consumers.map(consumer => (
-                                                <tr key={consumer._id}>
-                                                    <td className="px-4 py-2 border border-gray-300">
+                                            consumers.map((consumer, index) => (
+                                                <tr key={consumer._id} className={`${index % 2 === 0 ? 'bg-white' : 'bg-gray-100'}`}>
+                                                    <td className="px-4 font-medium py-2 border border-gray-300">
                                                         {consumer.shippingDetails.name}
                                                     </td>
-                                                    <td className="px-4 py-2 border border-gray-300">
+                                                    <td className="px-4 py-2 font-medium border border-gray-300">
                                                         {consumer.placedAt.split(' ')[0]}
                                                     </td>
-                                                    <td className="px-4 py-2 border border-gray-300">
+                                                    <td className="px-4 py-2 font-medium border border-gray-300">
                                                         {consumer.orderItems.map(item => (
-                                                            <div key={item.productId} className="flex justify-between items-center mb-4">
+                                                            <div key={item.productId} className="flex justify-between items-center mb-3">
                                                                 <div>{item.productName} x {item.quantity}</div>
                                                                 <div className="flex items-center">
                                                                     <select
@@ -164,7 +156,7 @@ function Orders() {
                                                                     </select>
                                                                     <button
                                                                         onClick={() => updateProductStatus(consumer.userId, item.productId)}
-                                                                        className="ml-2 px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+                                                                        className="ml-2 px-3 py-1 bg-orange-400 text-white rounded-lg hover:bg-orange-600 shadow-sm"
                                                                     >
                                                                         Update
                                                                     </button>
@@ -172,7 +164,7 @@ function Orders() {
                                                             </div>
                                                         ))}
                                                     </td>
-                                                    <td className="px-4 py-2 border border-gray-300">
+                                                    <td className="px-4 mb-10  items-center text-center py-2 font-medium border border-gray-300">
                                                         ${(consumer.orderItems || []).reduce((total, item) => total + (item.productPrice * item.quantity), 0).toFixed(2)}
                                                     </td>
                                                 </tr>
@@ -182,10 +174,6 @@ function Orders() {
                                 </table>
                             </div>
                         </div>
-                        <div className="text-xl font-bold"><br />
-                            <div className="font-light text-lg"></div>
-                        </div>
-                        <div className="mt-5"></div>
                     </div>
                 </div>
             </div>
