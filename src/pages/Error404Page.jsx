b6799c658b0f6e1b96aefb7c4fee199f37be8cce
Subasish Garai <subasishgarai41@gmail.com>
2024-10-17 15:08:39 +0530

    Browser Router fix

--- src/pages/Error404Page.jsx
+++ src/pages/Error404Page.jsx
@@ -10,7 +10,7 @@ export function Error404Page(){
                         </h2>
                         <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
                         <p className="mt-4 mb-8 dark:text-gray-600">But dont worry, you can find plenty of other things on our homepage.</p>
-                        <a rel="noopener noreferrer" href="/#/dashboard" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Back to homepage</a>
+                        <a rel="noopener noreferrer" href="/dashboard" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Back to homepage</a>
                     </div>
                 </div>
             </section>
