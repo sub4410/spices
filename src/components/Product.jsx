c8e8661af2e98977d0e3c4b4e6707b8e1d1b0244
Subasish Garai <158824896+sub4410@users.noreply.github.com>
2024-10-17 15:01:18 +0530

    Browser Router fix

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@ import nutmegt from '../assets/products/nutmeg.png';
 export default function ProductTile({ id, name, price, category, rating, brandName }) {
     return (
         <div className="relative group m-5 bg-gray-50 shadow-md rounded-lg overflow-hidden transition-transform duration-300 transform hover:scale-105">
-            <a href={`/#/productpage/${id}`}>
+            <a href={`/productpage/${id}`}>
             <div className="overflow-hidden aspect-w-1 aspect-h-1">
                 <img
                     className="object-cover w-full h-full transition-transform duration-300 group-hover:scale-110"
